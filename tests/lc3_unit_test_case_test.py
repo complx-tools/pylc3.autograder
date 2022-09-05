127b1e362a253ee6ef2fc4b800fae61f2258b7b6
Brandon <tricksterguy87@gmail.com>
2022-09-05 12:59:26 -0700

    Revert imports.

--- tests/lc3_unit_test_case_test.py
+++ tests/lc3_unit_test_case_test.py
@@ -8,9 +8,8 @@ import sys
 import unittest
 import zlib
 import pylc3.core
-
-from .. import pylc3.autograder.lc3_unit_test_case
-from .. import pylc3.autograder.lc3_unit_test_case.DataItem
+from pylc3.autograder import lc3_unit_test_case
+from pylc3.autograder import lc3_unit_test_case.DataItem
 
 
 # Note this file is not to be used as a template for writing student tests.
