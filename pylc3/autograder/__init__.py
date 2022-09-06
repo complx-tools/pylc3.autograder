2373e7f822a6cd409dd9daf72e488ec2ce6c7c95
Brandon <tricksterguy87@gmail.com>
2022-09-05 20:15:12 -0700

    Bump version: 0.1.1 → 0.1.2

--- pylc3/autograder/__init__.py
+++ pylc3/autograder/__init__.py
@@ -1,6 +1,6 @@
 """Autograder addon library for pylc3.core"""
 __author__ = """Brandon Whitehead"""
 __email__ = 'brandon.whitehead@gatech.edu'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from . import lc3_unit_test_case
